d395b0bd8c4575cf2396efe916a17fbd5dc4ddb4
Taylor Dawson <taylorjdawson@gmail.com>
2021-05-04 13:45:39 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,1 @@
-// import chains, { NetworkName } from './src'
-import jsonToTs from 'json-to-ts';
-import chains from './chains.json'
-console.log(jsonToTs(chains[0]))
-// console.log(chains.get(NetworkName['Ether-1']))
+export {default as chains} from './src'
